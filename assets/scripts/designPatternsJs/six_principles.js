fd2cbb1c00f837785811e39c3952fdc52ad7d440
KevenPotter <keven_potter@163.com>
2020-05-26 17:02:40 +0800

    Commit

--- assets/scripts/designPatternsJs/six_principles.js
+++ assets/scripts/designPatternsJs/six_principles.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     $('#design_patterns_page_button').click();
     overviewOfTheSixPrinciplesChart();
     sixPrinciples_ISP_type_of_interfaceChart();
+    design_patterns_overview_chart();
 });
 
 /**
@@ -172,6 +173,132 @@ function sixPrinciples_ISP_type_of_interfaceChart() {
                 data: [
                     {value: 1, name: '实例接口（Object Interface）'},
                     {value: 1, name: '类接口（Class Interface）'}
+                ],
+                label: {
+                    normal: {
+                        show: true,
+                        textStyle: {
+                            fontSize: 15
+                        }
+                    }
+                },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+    resize(myChart);
+}
+
+/**
+ * 设计模式概览图表
+ */
+function design_patterns_overview_chart() {
+    var design_patterns_overview_chart = $('#design_patterns_overview');
+    design_patterns_overview_chart.removeAttr("_echarts_instance_");
+    var myChart = echarts.init(design_patterns_overview_chart[0], 'macarons');
+    myChart.on('click', function (params) {
+        switch (params.name) {
+            case '单例模式':
+                layerCapture('design_patterns_singleton_pattern', 0, 40, 20);
+                break;
+            case '工厂方法模式':
+                layerCapture('design_patterns_factory_method_pattern', 0, 40, 30);
+                break;
+            case '抽象工厂模式':
+                layerCapture('design_patterns_abstract_factory_pattern', 0, 40, 30);
+                break;
+            case '模板方法模式':
+                layerCapture('design_patterns_template_method_pattern', 0, 40, 30);
+                break;
+            case '建造者模式':
+                layerCapture('design_patterns_builder_pattern', 0, 40, 30);
+                break;
+            case '代理模式':
+                layerCapture('design_patterns_proxy_pattern', 0, 40, 25);
+                break;
+            case '原型模式':
+                layerCapture('design_patterns_prototype_pattern', 0, 40, 25);
+                break;
+            case '中介者模式':
+                layerCapture('design_patterns_mediator_pattern', 0, 40, 30);
+                break;
+            case '命令模式':
+                layerCapture('design_patterns_command_pattern', 0, 40, 30);
+                break;
+        }
+    });
+    var option = null;
+    option = {
+        title: {
+            text: '设计模式概览',
+            subtext: '《设计模式之禅》-目录',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b} : {c} ({d}%)'
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                restore: {
+                    show: true
+                },
+                saveAsImage: {
+                    pixelRatio: 2
+                }
+            }
+        },
+        legend: {
+            bottom: 10,
+            left: 'center',
+            data: [
+                '单例模式', '工厂方法模式', '抽象工厂模式', '模板方法模式', '建造者模式', '代理模式', '原型模式', '中介者模式',
+                '命令模式', '责任链模式', '装饰模式', '策略模式', '适配器模式', '迭代器模式', '组合模式', '观察者模式',
+                '门面模式', '备忘录模式', '访问者模式', '状态模式', '解释器模式', '享元模式', '桥梁模式'],
+            textStyle: {
+                fontSize: 15
+            }
+        },
+        series: [
+            {
+                type: 'pie',
+                radius: '75%',
+                center: ['50%', '50%'],
+                selectedMode: 'single',
+                data: [
+                    {value: 1, name: '单例模式'},
+                    {value: 1, name: '工厂方法模式'},
+                    {value: 1, name: '抽象工厂模式'},
+                    {value: 1, name: '模板方法模式'},
+                    {value: 1, name: '建造者模式'},
+                    {value: 1, name: '代理模式'},
+                    {value: 1, name: '原型模式'},
+                    {value: 1, name: '中介者模式'},
+                    {value: 1, name: '命令模式'},
+                    {value: 1, name: '责任链模式'},
+                    {value: 1, name: '装饰模式'},
+                    {value: 1, name: '策略模式'},
+                    {value: 1, name: '适配器模式'},
+                    {value: 1, name: '迭代器模式'},
+                    {value: 1, name: '组合模式'},
+                    {value: 1, name: '观察者模式'},
+                    {value: 1, name: '门面模式'},
+                    {value: 1, name: '备忘录模式'},
+                    {value: 1, name: '访问者模式'},
+                    {value: 1, name: '状态模式'},
+                    {value: 1, name: '解释器模式'},
+                    {value: 1, name: '享元模式'},
+                    {value: 1, name: '桥梁模式'}
                 ],
                 label: {
                     normal: {
