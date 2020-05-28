db850af4db1364136191bee0277646fd137a1b7e
KevenPotter <keven_potter@163.com>
2020-05-28 17:13:25 +0800

    Commit

--- assets/scripts/designPatternsJs/factory_method_pattern.js
+++ assets/scripts/designPatternsJs/factory_method_pattern.js
@@ -3,14 +3,14 @@
  */
 $(document).ready(function () {
     $('#design_patterns_page_button').click();
-    // 单例模式的定义提示
-    singletonPatternDefinitionTips();
+    // 工厂方法模式的定义提示
+    factoryMethodPatternDefinitionTips();
     // 单例模式种类图表
     singletonPatternTypesChart();
-    // 单例模式通用类图放大镜
-    singletonPatternGenericClassDiagram();
-    // 单例模式的优缺点图表
-    the_advantages_and_disadvantages_of_the_singleton_pattern_chart();
+    // 工厂方法模式通用类图放大镜
+    factoryMethodPatternGenericClassDiagram();
+    // 工厂方法模式的优缺点图表
+    the_advantages_and_disadvantages_of_the_factory_method_pattern_chart();
 });
 
 /**
@@ -22,50 +22,36 @@ function singletonPatternTypesChart() {
     var myChart = echarts.init(singletonPatternTypesChart[0], 'macarons');
     myChart.on('click', function (params) {
         switch (params.name) {
-            case '静态常量':
-                layerCapture('singletonPattern_staticConstant', 0, 60, 70);
+            case 'Product':
+                layerCapture('factory_method_pattern_abstract_product', 0, 60, 40);
                 break;
-            case '静态代码块':
-                layerCapture('singletonPattern_staticCodeBlock', 0, 60, 70);
+            case 'ConcreteProduct1':
+                layerCapture('factory_method_pattern_concrete_product_1', 0, 60, 40);
                 break;
-            case '线程不安全':
-                layerCapture('singletonPattern_threadUnsafe', 0, 60, 70);
+            case 'ConcreteProduct2':
+                layerCapture('factory_method_pattern_concrete_product_2', 0, 60, 40);
                 break;
-            case '同步方法（线程安全）':
-                layerCapture('singletonPattern_synchronizationMethod', 0, 60, 70);
+            case 'Creator':
+                layerCapture('factory_method_pattern_abstract_creator', 0, 60, 40);
                 break;
-            case '同步代码块（线程安全）':
-                layerCapture('singletonPattern_synchronousCodeBlock', 0, 60, 70);
+            case 'ConcreteCreator':
+                layerCapture('factory_method_pattern_concrete_creator', 0, 60, 40);
                 break;
-            case '双重检查':
-                layerCapture('singletonPattern_doubleCheck', 0, 60, 70);
-                break;
-            case '静态内部类':
-                layerCapture('singletonPattern_staticInnerClass', 0, 60, 60);
-                break;
-            case '枚举':
-                layerCapture('singletonPattern_enumerate', 0, 60, 60);
+            case 'Client':
+                layerCapture('factory_method_pattern_client', 0, 60, 70);
                 break;
         }
     });
     var option = null;
     option = {
         title: {
-            text: '单例模式种类',
-            subtext: '8种',
+            text: '工厂方法模式通用模板',
+            subtext: '《设计模式之禅》-P69',
             left: 'center'
         },
         tooltip: {
             trigger: 'item',
-            formatter: '{b} : {c} ({d}%)'
-        },
-        legend: {
-            left: 'center',
-            top: 'bottom',
-            data: ['静态常量', '静态代码块', '线程不安全', '同步方法（线程安全）', '同步代码块（线程安全）', '双重检查', '静态内部类', '枚举'],
-            textStyle: {
-                fontSize: 15
-            }
+            formatter: '{b}'
         },
         toolbox: {
             show: true,
@@ -78,23 +64,47 @@ function singletonPatternTypesChart() {
                 }
             }
         },
-        series: [
-            {
-                name: '单例模式种类',
-                type: 'pie',
-                roseType: 'area',
-                data: [
-                    {value: 3, name: '静态常量'},
-                    {value: 3, name: '静态代码块'},
-                    {value: 1, name: '线程不安全'},
-                    {value: 2, name: '同步方法（线程安全）'},
-                    {value: 2, name: '同步代码块（线程安全）'},
-                    {value: 4, name: '双重检查'},
-                    {value: 4, name: '静态内部类'},
-                    {value: 5, name: '枚举'}
-                ]
+        series: [{
+            type: 'treemap',
+            data: [{
+                name: 'Product',
+                value: 7,
+                children: [{
+                    name: 'Product',
+                    value: 2
+                }, {
+                    name: 'ConcreteProduct1',
+                    value: 3
+                }, {
+                    name: 'ConcreteProduct2',
+                    value: 2
+                }]
+            }, {
+                name: 'Creator',
+                value: 3,
+                children: [{
+                    name: 'Creator',
+                    value: 1
+                }, {
+                    name: 'ConcreteCreator',
+                    value: 2
+                }]
+            }, {
+                name: 'Client',
+                value: 1,
+                children: [{
+                    name: 'Client',
+                    value: 1
+                }]
+            }],
+            "label": {
+                "normal": {
+                    "textStyle": {
+                        "fontSize": 20
+                    }
+                }
             }
-        ]
+        }]
     };
     if (option && typeof option === "object") {
         myChart.setOption(option, true);
@@ -103,19 +113,19 @@ function singletonPatternTypesChart() {
 }
 
 /**
- * 单例模式的定义提示
+ * 工厂方法模式的定义提示
  */
-function singletonPatternDefinitionTips() {
-    $('#singleton_pattern_definition_tips').mouseover(function () {
-        layer.tips('确保某一个类只有一个实例，而且自行实例化并向整个系统提供这个实例。', $('#singleton_pattern_definition_tips'), {tips: 3});
+function factoryMethodPatternDefinitionTips() {
+    $('#factory_method_pattern_definition_tips').mouseover(function () {
+        layer.tips('定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类。', $('#factory_method_pattern_definition_tips'), {tips: 3});
     });
 }
 
 /**
- * 单例模式通用类图放大镜
+ * 工厂方法模式通用类图放大镜
  */
-function singletonPatternGenericClassDiagram() {
-    $('#singleton_pattern_generic_class_diagram').blowup({
+function factoryMethodPatternGenericClassDiagram() {
+    $('#factory_method_pattern_generic_class_diagram').blowup({
         "cursor": false,
         "width": 300,
         "height": 300
@@ -123,42 +133,42 @@ function singletonPatternGenericClassDiagram() {
 }
 
 /**
- * 单例模式的优缺点图表
+ * 工厂方法模式的优缺点图表
  */
-function the_advantages_and_disadvantages_of_the_singleton_pattern_chart() {
-    var the_advantages_and_disadvantages_of_the_singleton_pattern_chart = $('#the_advantages_and_disadvantages_of_the_singleton_pattern');
-    the_advantages_and_disadvantages_of_the_singleton_pattern_chart.removeAttr("_echarts_instance_");
-    var myChart = echarts.init(the_advantages_and_disadvantages_of_the_singleton_pattern_chart[0], 'macarons');
+function the_advantages_and_disadvantages_of_the_factory_method_pattern_chart() {
+    var the_advantages_and_disadvantages_of_the_factory_method_pattern_chart = $('#the_advantages_and_disadvantages_of_the_factory_method_pattern');
+    the_advantages_and_disadvantages_of_the_factory_method_pattern_chart.removeAttr("_echarts_instance_");
+    var myChart = echarts.init(the_advantages_and_disadvantages_of_the_factory_method_pattern_chart[0], 'macarons');
     myChart.on('click', function (params) {
         switch (params.name) {
-            case '减少内存开支':
-                layerCapture('singleton_mode_advantages_reduce_memory_expenses', 10000, 40, 20);
+            case '良好的封装性':
+                layerCapture('factory_method_mode_advantages_good_encapsulation', 10000, 40, 20);
                 break;
-            case '减少系统性能开销':
-                layerCapture('singleton_mode_advantages_reduce_system_performance_overhead', 10000, 40, 20);
+            case '优秀的扩展性':
+                layerCapture('factory_method_mode_advantages_excellent_scalability', 10000, 40, 20);
                 break;
-            case '避免资源多重占用':
-                layerCapture('singleton_mode_advantages_avoid_multiple_resources', 10000, 40, 20);
+            case '屏蔽产品类':
+                layerCapture('factory_method_mode_advantages_shielding_products', 10000, 40, 20);
                 break;
-            case '优化共享资源访问':
-                layerCapture('singleton_mode_advantages_avoid_multiple_resources_optimize_access_to_shared_resources', 10000, 40, 20);
+            case '典型的解耦框架':
+                layerCapture('factory_method_mode_advantages_typical_decoupling_framework', 10000, 40, 20);
                 break;
-            case '扩展困难':
-                layerCapture('singleton_pattern_disadvantages_difficult_to_expand', 10000, 40, 20);
-                break;
-            case '对测试不利':
-                layerCapture('singleton_pattern_disadvantages_not_good_for_testing', 10000, 40, 20);
-                break;
-            case '与SRP有冲突':
-                layerCapture('singleton_pattern_disadvantages_conflict_with_SRP', 10000, 40, 20);
-                break;
+            // case '扩展困难':
+            //     layerCapture('singleton_pattern_disadvantages_difficult_to_expand', 10000, 40, 20);
+            //     break;
+            // case '对测试不利':
+            //     layerCapture('singleton_pattern_disadvantages_not_good_for_testing', 10000, 40, 20);
+            //     break;
+            // case '与SRP有冲突':
+            //     layerCapture('singleton_pattern_disadvantages_conflict_with_SRP', 10000, 40, 20);
+            //     break;
         }
     });
     var option = null;
     option = {
         title: {
-            text: '单例模式的优缺点',
-            subtext: '《设计模式之禅》-P60',
+            text: '工厂方法模式的优缺点',
+            subtext: '《设计模式之禅》-P70',
             left: 'center'
         },
         tooltip: {
@@ -168,7 +178,7 @@ function the_advantages_and_disadvantages_of_the_singleton_pattern_chart() {
         legend: {
             left: 'center',
             top: 'bottom',
-            data: ['减少内存开支', '减少系统性能开销', '避免资源多重占用', '优化共享资源访问', '扩展困难', '对测试不利', '与SRP有冲突'],
+            data: ['良好的封装性', '优秀的扩展性', '屏蔽产品类', '典型的解耦框架'],
             textStyle: {
                 fontSize: 15
             }
@@ -199,34 +209,34 @@ function the_advantages_and_disadvantages_of_the_singleton_pattern_chart() {
                     }
                 },
                 data: [
-                    {value: 1, name: '减少内存开支'},
-                    {value: 2, name: '减少系统性能开销'},
-                    {value: 3, name: '避免资源多重占用'},
-                    {value: 4, name: '优化共享资源访问'}
+                    {value: 1, name: '良好的封装性'},
+                    {value: 2, name: '优秀的扩展性'},
+                    {value: 3, name: '屏蔽产品类'},
+                    {value: 4, name: '典型的解耦框架'}
                 ]
             },
-            {
-                name: '缺点',
-                type: 'pie',
-                radius: [30, 110],
-                center: ['75%', '50%'],
-                roseType: 'area',
-                data: [
-                    {value: 1, name: '扩展困难'},
-                    {value: 2, name: '对测试不利'},
-                    {value: 3, name: '与SRP有冲突'}
-                ]
-            }
+            // {
+            //     name: '缺点',
+            //     type: 'pie',
+            //     radius: [30, 110],
+            //     center: ['75%', '50%'],
+            //     roseType: 'area',
+            //     data: [
+            //         {value: 1, name: '扩展困难'},
+            //         {value: 2, name: '对测试不利'},
+            //         {value: 3, name: '与SRP有冲突'}
+            //     ]
+            // }
         ]
     };
     if (option && typeof option === "object") {
         myChart.setOption(option, true);
     }
     resize(myChart);
-    $('#singleton_pattern_generic_class_diagram_panel_body')
-        .css('padding-top', $('#the_advantages_and_disadvantages_of_the_singleton_pattern_panel_body').height() / 4)
-        .css('padding-left', $('#the_advantages_and_disadvantages_of_the_singleton_pattern_panel_body').width() / 5)
-        .css('height', $('#the_advantages_and_disadvantages_of_the_singleton_pattern_panel_body').height());
+    $('#factory_method_pattern_generic_class_diagram_panel_body')
+        .css('padding-top', $('#the_advantages_and_disadvantages_of_the_factory_method_pattern_panel_body').height() / 4)
+        .css('padding-left', $('#the_advantages_and_disadvantages_of_the_factory_method_pattern_panel_body').width() / 5)
+        .css('height', $('#the_advantages_and_disadvantages_of_the_factory_method_pattern_panel_body').height());
 }
 
 /**
