0053043d1c98117342050fa198251387c1a68675
KevenPotter <keven_potter@163.com>
2020-12-14 17:05:26 +0800

    Commit

--- assets/scripts/mysql/mysql-high-level-knowledge/mysql-high-level-knowledge-mysql-log-and-performance-optimization.js
+++ assets/scripts/mysql/mysql-high-level-knowledge/mysql-high-level-knowledge-mysql-log-and-performance-optimization.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     $('#database_page_button').click();
     $('#database_page_high_level_button').click();
     logClassificationChart();
+    analyzeQueryStatementsChart();
 });
 
 /**
@@ -157,4 +158,113 @@ function logClassificationSlowQueryLogTips() {
  */
 function logClassificationIsMoreDetailedTips() {
     layerCapture('log_classification_is_more_detailed_tips', 0, 50, 52);
+}
+
+/**
+ * 分析查询语句提示
+ */
+function analyzeQueryStatementsTips() {
+    layerCapture('analyze_query_statements_tips', 0, 40, 64);
+}
+
+/**
+ * 分析查询语句图表
+ */
+function analyzeQueryStatementsChart() {
+    var analyzeQueryStatementsChart = $('#analyze_query_statements');
+    analyzeQueryStatementsChart.removeAttr("_echarts_instance_");
+    var myChart = echarts.init(analyzeQueryStatementsChart[0], 'macarons');
+    myChart.on('click', function (params) {
+        switch (params.name) {
+            case 'id':
+                layerCapture('analyze_query_statements_explain_id', 0, 50, 13);
+                break;
+            case 'select_type':
+                layerCapture('analyze_query_statements_explain_select_type', 0, 50, 44);
+                break;
+            case 'table':
+                layerCapture('analyze_query_statements_explain_table', 0, 50, 13);
+                break;
+            case 'type':
+                layerCapture('analyze_query_statements_explain_type', 0, 50, 70);
+                break;
+        }
+    });
+    var option = null;
+    option = {
+        title: {
+            text: '分析查询语句',
+            subtext: '《MySQL8从入门到精通（视频教学版）》-P419',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b} : {c} ({d}%)'
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                restore: {
+                    show: true
+                },
+                saveAsImage: {
+                    pixelRatio: 2
+                }
+            }
+        },
+        legend: {
+            bottom: 10,
+            left: 'center',
+            data: ['id', 'select_type', 'table', 'type', 'possible_keys', 'key', 'key_len', 'ref', 'rows', 'Extra'],
+            textStyle: {
+                fontSize: 15
+            }
+        },
+        series: [
+            {
+                type: 'pie',
+                radius: '75%',
+                center: ['50%', '50%'],
+                selectedMode: 'single',
+                data: [
+                    {value: 1, name: 'id'},
+                    {value: 1, name: 'select_type'},
+                    {value: 1, name: 'table'},
+                    {value: 1, name: 'type'},
+                    {value: 1, name: 'possible_keys'},
+                    {value: 1, name: 'key'},
+                    {value: 1, name: 'key_len'},
+                    {value: 1, name: 'ref'},
+                    {value: 1, name: 'rows'},
+                    {value: 1, name: 'Extra'}
+                ],
+                label: {
+                    normal: {
+                        show: true,
+                        textStyle: {
+                            fontSize: 15
+                        }
+                    }
+                },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+    resize(myChart);
+}
+
+/**
+ * 分析查询语句-EXPLAIN-type-const
+ */
+function analyzeQueryStatementsExplainTypeConstTips() {
+    layerCapture('analyze_query_statements_explain_type_const_tips', 0, 40, 27);
 }
